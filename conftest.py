dd327a90ffea3aa2e4f5198076b1b8d26f26bb34
milosz.hoc <miloszhoc@gmail.com>
2021-05-01 21:34:51 +0200

    fixed html raport

--- conftest.py
+++ conftest.py
@@ -14,19 +14,35 @@ def pytest_runtest_makereport(item, call):
         xfail = hasattr(report, "wasxfail")
         if (report.skipped and xfail) or (report.failed and not xfail):
             # only add additional html on failure
-            web_driver = item.funcargs['get_driver']
+            web_driver = item.funcargs['setup_browser']
             image = web_driver.get_screenshot_as_base64()
             extra.append(pytest_html.extras.image(image))
         report.extra = extra
 
 
-@pytest.fixture(autouse=True, scope='function')
+@pytest.fixture(autouse=True, scope='module')
 def setup_browser():
     driver = CreateDriver()
-    driver.set_driver('chrome', 'remote', '--headless', driver_path='drivers/chromedriver.exe')
+    # driver.set_driver('chrome', 'local', './drivers/chromedriver.exe', '--headless')
+    driver.set_driver('chrome', 'local', './drivers/chromedriver.exe')
     browser = driver.get_current_driver()
-    browser.get(APP_URL)
 
     yield browser
 
     browser.quit()
+
+
+@pytest.fixture(scope='function')
+def get_client_page(setup_browser):
+    browser = setup_browser
+    browser.get(APP_URL)
+
+    return browser
+
+
+@pytest.fixture(scope='function')
+def get_worker_page(setup_browser):
+    browser = setup_browser
+    browser.get(APP_URL + '/worker')
+
+    return browser
