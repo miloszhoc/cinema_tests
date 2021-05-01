a3d32c23e31b5ca07bc59aa27ed9995ae860d46e
milosz.hoc <miloszhoc@gmail.com>
2021-05-01 23:23:04 +0200

    updated worker panel test cases

--- tests/worker/test_login.py
+++ tests/worker/test_login.py
@@ -19,9 +19,36 @@ def test_login_as_staff_admin(get_worker_page):
         assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Typy biletów'))
     page.logout()
 
-# def test_login_as_admin(login_logout):
-#     pass
-#
-#
-# def test_login_as_worker(login_logout):
-#     pass
+
+def test_login_as_admin(get_worker_page):
+    driver = get_worker_page
+    page = PreLoginP(driver)
+    page = page.enter_login_page().login(ADMIN_LOG, ADMIN_PASS)
+    with pytest.assume:
+        assert ADMIN_LOG in page.get_text(page.TEXT_USERNAME_L)
+    with pytest.assume:
+        assert not page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Zarządzanie użytkownikami'))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Seanse'))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Filmy'))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Typy biletów'))
+    page.logout()
+
+
+def test_login_as_worker(get_worker_page):
+    driver = get_worker_page
+    page = PreLoginP(driver)
+    page = page.enter_login_page().login(WORKER_LOG, WORKER_PASS)
+    with pytest.assume:
+        assert WORKER_LOG in page.get_text(page.TEXT_USERNAME_L)
+    with pytest.assume:
+        assert not page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Zarządzanie użytkownikami'))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Seanse'))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Filmy'))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.HREF_MODULE_L_D, 'Typy biletów'))
+    page.logout()
