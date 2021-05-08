424e94b3ba71d8643f7738e9d8304a85dd601423
milosz.hoc <miloszhoc@gmail.com>
2021-05-09 00:54:52 +0200

    added tests for ticket type module

---
+++ tests/worker/ticket_types/test_ticket_type_add_check.py
@@ -0,0 +1,38 @@
+import pytest
+
+from env_data import STAFF_ADMIN_LOG, STAFF_ADMIN_PASS
+from pom.worker.pages.ticket_types.list_page import ActiveTicketTypesListP
+from utils.utils import DateUtils
+
+
+@pytest.mark.parametrize('is_deleted', [True, False])
+def test_add_ticket_type_and_check_details(login_logout, is_deleted):
+    browser = login_logout(STAFF_ADMIN_LOG, STAFF_ADMIN_PASS, '/worker/typy-biletow')
+    page = ActiveTicketTypesListP(browser)
+
+    type_name = DateUtils.add_timestamp('test ticket type')
+    price = '12.30'
+    conditions = 'test conditions'
+
+    if is_deleted:
+        deleted = True
+    else:
+        deleted = False
+
+    page = page.open_add_ticket_type_form().add_ticket_type(type_name, price, conditions, deleted)
+
+    with pytest.assume:
+        assert page.check_url('typy-biletow')
+
+    if is_deleted:
+        page = page.open_deleted_ticket_types_list()
+    page = page.open_ticket_type_details(type_name)
+
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.TEXT_FIELD_NAME_VALUE_D, name='Typ', value=type_name))
+    with pytest.assume:
+        assert page.is_element_on_page(
+            page.dynamic_locator(page.TEXT_FIELD_NAME_VALUE_D, name='Cena', value=price.replace('.', ',')))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.TEXT_FIELD_NAME_VALUE_D,
+                                                            name='Dodatkowe informacje', value=conditions))
---
+++ tests/worker/ticket_types/test_ticket_type_delete.py
@@ -0,0 +1,22 @@
+import pytest
+
+from env_data import STAFF_ADMIN_LOG, STAFF_ADMIN_PASS
+from pom.worker.pages.ticket_types.list_page import ActiveTicketTypesListP
+from utils.utils import DateUtils
+
+
+def test_delete_ticket_type(login_logout):
+    browser = login_logout(STAFF_ADMIN_LOG, STAFF_ADMIN_PASS, '/worker/typy-biletow')
+    page = ActiveTicketTypesListP(browser)
+
+    type_name = DateUtils.add_timestamp('ticket type to delete')
+    price = '33.30'
+    conditions = 'test conditions'
+
+    page = page.open_add_ticket_type_form().add_ticket_type(type_name, price, conditions, False)
+    page = page.open_ticket_type_details(type_name)
+    page = page.open_delete_ticket_type_page()
+    with pytest.assume:
+        assert page.check_ticket_type_name(type_name)
+    page = page.delete_ticket_type()
+    assert not page.is_element_on_page(page.dynamic_locator(page.HREF_TICKET_TYPE_DETAILS_D, type_name))
