869eaeefc049ac3d4acc2ca915e9d82a921139c9
milosz.hoc <miloszhoc@gmail.com>
2021-05-17 00:25:44 +0200

    methods for working with dates

--- utils/utils.py
+++ utils/utils.py
@@ -12,3 +12,24 @@ class DateUtils:
         :return: New string
         """
         return '{}'.format(sep).join((text, str(datetime.datetime.now().timestamp()).split('.')[0]))
+
+    @staticmethod
+    def get_current_date() -> str:
+        """
+        :return: Current date in YY-MM-DD format
+        """
+        return datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+
+    @staticmethod
+    def get_current_time() -> str:
+        """
+        :return: Current time in HH:MM format
+        """
+        return datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
+
+    @staticmethod
+    def get_current_datetime() -> str:
+        """
+        :return: Current datetime in 'YY-MM-DD HH:MM' format
+        """
+        return datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M')
