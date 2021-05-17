a345a658d284468202248773908681af9e2aa666
milosz.hoc <miloszhoc@gmail.com>
2021-05-17 23:00:37 +0200

    tests for create and check film show details

---
+++ tests/worker/film_screening/test_film_screening_add_check.py
@@ -0,0 +1,45 @@
+import pytest
+
+from env_data import STAFF_ADMIN_PASS, STAFF_ADMIN_LOG
+from pom.worker.pages.film_show.list_page import ActiveFilmShowListP
+from utils.utils import DateUtils
+
+
+def test_add_film_show_and_check_details(create_movie, login_logout):
+    browser = login_logout(STAFF_ADMIN_LOG, STAFF_ADMIN_PASS, '/worker/seanse/')
+    movie_data = create_movie
+
+    movie_title = movie_data['title']
+    start_datetime = DateUtils.get_current_datetime(minutes=15)
+    break_time = '00:15:00'
+
+    page = ActiveFilmShowListP(browser)
+    page = page.open_add_film_show_form().add_film_show(movie_title, start_datetime, break_time)
+
+    with pytest.assume:
+        assert page.check_url('szczegoly-seansu')
+
+    page = page.open_panel_page().open_module('Seanse').open_film_show_details(movie_title)
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.TEXT_FIELD_NAME_VALUE_D,
+                                                            name='Tytuł filmu', value=movie_title))
+
+    start_date, start_time = start_datetime.replace('-', '.').split(' ')
+    start_date = start_date.split('.')[2] + '.' + start_date.split('.')[1] + '.' + start_date.split('.')[0]
+    start_datetime = start_date + 'r. ' + start_time
+
+    end_datetime = DateUtils.count_end_time(start_date, start_time, movie_data['duration'], break_time)
+    end_date = end_datetime.split(' ')[0]
+    end_time = end_datetime.split(' ')[1]
+    end_datetime = end_date + 'r. ' + end_time
+    total_time = start_datetime + ' - ' + end_datetime
+
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.TEXT_FIELD_NAME_VALUE_D,
+                                                            name='Data seansu', value=total_time))
+    with pytest.assume:
+        assert page.is_element_on_page(page.dynamic_locator(page.TEXT_FIELD_NAME_VALUE_D,
+                                                            name='Czas trwania', value=movie_data['duration']))
+
+    with pytest.assume:
+        assert page.is_element_on_page(page.HREF_ADD_RESERVATION_S)
