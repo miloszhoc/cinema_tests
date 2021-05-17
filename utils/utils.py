cd64f338e2428be29d5333f6f345affcd716deca
milosz.hoc <miloszhoc@gmail.com>
2021-05-17 22:58:26 +0200

    function for counting show end time

--- utils/utils.py
+++ utils/utils.py
@@ -28,8 +28,29 @@ class DateUtils:
         return datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
 
     @staticmethod
-    def get_current_datetime() -> str:
+    def get_current_datetime(days: float = 0, hours: float = 0, minutes: float = 0, seconds: float = 0) -> str:
         """
         :return: Current datetime in 'YY-MM-DD HH:MM' format
         """
-        return datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M')
+        date = datetime.datetime.now() + datetime.timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
+        return datetime.datetime.strftime(date, '%Y-%m-%d %H:%M')
+
+    @staticmethod
+    def count_end_time(start_date: str, start_time: str, *args: str) -> str:
+        """
+        Counts end datetime.
+
+        :param args: list of duration in 'HH:MM:SS' format
+        :param start_date: date in DD.MM.YYYY
+        :param start_time: time in HH:MM
+        :return: calculated end time
+        """
+        start_datetime = datetime.datetime.strptime(start_date + ' ' + start_time, '%d.%m.%Y %H:%M')
+        datetime.timedelta()
+        end_time = start_datetime
+        for duration in args:
+            hours = float(duration.split(':')[0])
+            minutes = float(duration.split(':')[1])
+            seconds = float(duration.split(':')[2])
+            end_time += datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        return datetime.datetime.strftime(end_time, '%d.%m.%Y %H:%M')
