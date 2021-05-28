76345dc201f155b6551fa945721312409f291f0d
milosz.hoc <miloszhoc@gmail.com>
2021-05-28 23:28:38 +0200

    updated add reservation test

--- tests/worker/reservations/test_add_reservation.py
+++ tests/worker/reservations/test_add_reservation.py
@@ -1,22 +1,25 @@
 import pytest
 from selenium.common.exceptions import TimeoutException
 
-from env_data import STAFF_ADMIN_LOG, STAFF_ADMIN_PASS
+from env_data import STAFF_ADMIN_LOG, STAFF_ADMIN_PASS, EMAIL_LOGIN
 from pom.worker.pages.film_show.list_page import ActiveFilmShowListP
 
+from utils.mail import Email
+from utils.utils import DateUtils
 
-# @pytest.mark.email
-def test_add_reservation_to_active_film_show(create_active_film_show, create_ticket_type, login_logout):
+
+@pytest.mark.email
+def test_add_check_reservation_in_active_film_show(create_active_film_show, create_ticket_type, login_logout):
     browser = login_logout(STAFF_ADMIN_LOG, STAFF_ADMIN_PASS, '/worker/seanse')
     show_data = create_active_film_show
     ticket_type = create_ticket_type
 
     page = ActiveFilmShowListP(browser).open_film_show_details(show_data['movie_title'])
     page = page.open_add_reservation_form()
-    name = 'John'
+    name = DateUtils().add_timestamp('John')
     last_name = 'Test'
     number = '789456123'
-    email = 'johntest@example.com'
+    email = EMAIL_LOGIN
     seats = ['A1', 'C2']
     page.type_name(name)
     page.type_last_name(last_name)
@@ -57,7 +60,26 @@ def test_add_reservation_to_active_film_show(create_active_film_show, create_tic
     assert page.check_message(
         'Rezerwacja została pomyślnie utworzona, na adres mailowy klienta została wysłana wiadomość z potwierdzeniem. Jeśli klient nie potwierdzi rezerwacji w ciągu 30 minut, to zostanie ona usunięta z systemu')
 
-    # todo check email and if reservation appears on list
+    e = Email()
+    email_content = e.wait_open_email('Potwierdzenie rezerwacji')
+
+    assert '/potwierdz/' in email_content
+    assert '/anuluj/' in email_content
+
+    full_name = name + ' ' + last_name
+    page.open_reservation_details(full_name)
+
+    for seat in seats:
+        with pytest.assume:
+            assert page.check_reservation_details(full_name, 'Miejsce', seat[1] + '' + seat[0])
+        with pytest.assume:
+            assert page.check_reservation_details(full_name, 'Miejsce', ticket_type['name'])
+        with pytest.assume:
+            assert page.check_reservation_details(full_name, 'Miejsce', str(ticket_price).replace('.', ','))
+    with pytest.assume:
+        assert page.check_reservation_details(full_name, 'Email', email)
+    with pytest.assume:
+        assert page.check_reservation_details(full_name, 'Telefon', number)
 
 
 def test_add_reservation_to_archive_film_show(create_archived_film_show, login_logout):
