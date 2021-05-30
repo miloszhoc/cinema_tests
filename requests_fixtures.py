9dbcbeba968149e1a3a2b581713af948b5682720
milosz.hoc <miloszhoc@gmail.com>
2021-05-30 23:44:10 +0200

    added ticket type name to fixture response

--- requests_fixtures.py
+++ requests_fixtures.py
@@ -109,6 +109,7 @@ def create_film_show_with_reservation(create_active_film_show, create_ticket_typ
     confirmation_email = False
 
     data = {'movie_title': film_show['movie_title'],
+            'ticket_type_name': ticket_type['name'],
             'first_name': first_name,
             'last_name': last_name,
             'email': email,
