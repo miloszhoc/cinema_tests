a90c5862193886df47326439dba6892ee39a27cc
milosz.hoc <miloszhoc@gmail.com>
2021-05-31 00:09:05 +0200

    added email credentials

--- env_data.py
+++ env_data.py
@@ -11,6 +11,11 @@ else:
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+EMAIL_LOGIN = os.environ.get('email_login')
+EMAIL_PASSWD = os.environ.get('email_passwd')
+EMAIL_HOST = os.environ.get('email_host')
+EMAIL_PORT = os.environ.get('email_port')
+
 SELENIUM_HUB_URL = config['global']['SELENIUM_HUB_URL']
 APP_URL = config[environment]['APP_URL']
 ADMIN_LOG = config[environment]['ADMIN_LOG']
