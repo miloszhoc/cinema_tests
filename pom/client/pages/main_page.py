bc317a91d6e0d04ba22d4cf2e6f15242e45f0edc
milosz.hoc <miloszhoc@gmail.com>
2021-06-05 23:33:09 +0200

    main page pom check message

--- pom/client/pages/main_page.py
+++ pom/client/pages/main_page.py
@@ -5,7 +5,17 @@ from pom.client.pages.top_menu import TopMenuP
 
 
 class MainP(TopMenuP):
+    TEXT_MESSAGE_S = (By.CLASS_NAME, 'message')
     HREF_MOVIE_DETAILS_D = (By.XPATH, '//a/div[contains(text(), "{}")]')
+
+    def check_message(self, message: str) -> bool:
+        """
+        Checks message which appears on screen
+
+        :param message: message content
+        :return: True or false
+        """
+        return message in self.get_text(self.TEXT_MESSAGE_S)
 
     def open_movie_details(self, movie_title: str):
         """
