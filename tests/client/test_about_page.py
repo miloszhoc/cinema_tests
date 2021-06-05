cd22c567e8a1fa495cdef075d341965f03aa9a1c
milosz.hoc <miloszhoc@gmail.com>
2021-06-06 00:08:16 +0200

    about page tests

---
+++ tests/client/test_about_page.py
@@ -0,0 +1,9 @@
+from pom.client.pages.main_page import MainP
+
+
+def test_about_page(get_client_module):
+    driver = get_client_module
+    page = MainP(driver)
+    page = page.open_about_page()
+    assert page.get_page_title() == 'O kinie'
+    assert page.get_number_of_cards() == 3
