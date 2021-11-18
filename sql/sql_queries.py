20b145b158c64049a1fc9ee68d15d3aaea3762f8
milosz.hoc <miloszhoc@gmail.com>
2021-11-18 22:38:33 +0100

    sql queries

---
+++ sql/sql_queries.py
@@ -0,0 +1,7 @@
+DELETE_CLIENT = '''DELETE FROM worker_client WHERE client_id={};'''
+DELETE_FILM_SHOW = '''DELETE FROM worker_showtime WHERE showtime_id={};'''
+DELETE_MOVIE = '''DELETE FROM worker_movie WHERE movie_id={};'''
+DELETE_RESERVATION = '''DELETE FROM worker_reservation WHERE reservation_id={};'''
+DELETE_RESERVATION_TICKET = '''DELETE FROM worker_reservation_ticket_id WHERE id={};'''
+DELETE_TICKET = '''DELETE FROM worker_ticket WHERE ticket_id={};'''
+DELETE_TICKETTYPE = '''DELETE FROM worker_tickettype WHERE ticket_id={};'''
